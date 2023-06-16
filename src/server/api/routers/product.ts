28780ac1011b5126250be1ab796ded4c9b1e2bc9
Bruno-mateus <bruno_205@hotmail.com>
2023-06-16 11:38:11 -0300

    create backend

--- src/server/api/routers/product.ts
+++ src/server/api/routers/product.ts
@@ -4,6 +4,7 @@ import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 const LanguageSchema = z.object({
   languageCode: z.string(),
+  name:z.string(),
   translation: z.object({
     name: z.string(),
     subtitle: z.string(),
@@ -19,6 +20,12 @@ const ProductSchema = z.object({
   translations: z.array(LanguageSchema),
 });
 
+const TranslationSchema = z.object({
+  productId:z.number(),
+  language:LanguageSchema,
+
+})
+
 export const productRouter = createTRPCRouter({
   create: publicProcedure
     .input(ProductSchema)
@@ -32,5 +39,57 @@ export const productRouter = createTRPCRouter({
         },
      });
       return product;
-    })
+    }),
+    createTranslateProduct:publicProcedure.input(TranslationSchema).mutation(
+      async ({input,ctx})=>{
+        const product = await ctx.db.product.findFirst({
+          where:{
+            id:input.productId
+          }
+        })
+
+        if(!product){
+          throw new Error("product not found")
+        }
+
+        const language = await ctx.db.language.findFirst({
+          where:{
+            code:input.language.languageCode
+          }
+        })
+
+        if(!language){
+          const newLanguage = await ctx.db.language.create({
+            data:{
+              code:input.language.languageCode,
+              name:input.language.name
+            }
+          })
+          
+        const translation = await ctx.db.translation.create({
+          data:{
+            description:input.language.translation.description,
+            name:input.language.translation.name,
+            subtitle:input.language.translation.subtitle,
+            languageId:newLanguage.id,
+            productId:product.id
+          }
+        })
+
+        return translation
+        }
+
+
+        const translation = await ctx.db.translation.create({
+          data:{
+            description:input.language.translation.description,
+            name:input.language.translation.name,
+            subtitle:input.language.translation.subtitle,
+            languageId:language.id,
+            productId:product.id
+          }
+        })
+        return translation
+      }
+    )
 });
